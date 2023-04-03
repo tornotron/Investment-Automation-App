f2a46214108660d9abf63473a90897ad7fb84990
kesh-hrishi <hrishikeshajith0@gmail.com>
2023-04-03 20:11:44 +0530

    Checking for positive cash flow

---
+++ alpha.py
@@ -0,0 +1,16 @@
+from alpha_vantage.timeseries import TimeSeries
+import time
+
+ts=TimeSeries(key='12TJNFS3NSMQO9M5')
+symbols = ['AAPL', 'GOOG', 'MSFT', 'AMZN']
+
+for symbol in symbols:
+    cash_flow_data, cash_flow_metadata = ts.CASH_FLOW(symbol=symbol)
+    operating_cash_flow = float( cash_flow_data['quarterlyReports'][0]['operatingCashflow'])
+    if operating_cash_flow > 0:
+        print(f"Stock symbol: {symbol}")
+        print(f"Operating cash flow: {operating_cash_flow}")
+        print("")
+
+time.sleep(15)
+
