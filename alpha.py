7b352e4dfd31322ddb19c95a219cc90b681d768d
Abhijith Anandakrishnan <anandarkishnanabhijith@gmail.com>
2023-04-03 21:04:55 +0530

    Corrected Formatting

--- alpha.py
+++ alpha.py
@@ -2,16 +2,17 @@ from alpha_vantage.timeseries import TimeSeries
 from dotenv import load_dotenv
 import time
 
-ts=TimeSeries(key='API_KEY')
-symbols = ['AAPL', 'GOOG', 'MSFT', 'AMZN']
+ts = TimeSeries(key="API_KEY")
+symbols = ["AAPL", "GOOG", "MSFT", "AMZN"]
 
 for symbol in symbols:
     cash_flow_data, cash_flow_metadata = ts.cash_flow(symbol=symbol)
-    operating_cash_flow = float( cash_flow_data['quarterlyReports'][0]['operatingCashflow'])
+    operating_cash_flow = float(
+        cash_flow_data["quarterlyReports"][0]["operatingCashflow"]
+    )
     if operating_cash_flow > 0:
         print(f"Stock symbol: {symbol}")
         print(f"Operating cash flow: {operating_cash_flow}")
         print("")
 
 time.sleep(15)
-
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
-app= FastAPI()
+app = FastAPI()
+
 
 @app.get("/")
 async def root():
