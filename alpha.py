cc23c130f2fc40905080a218a4ffa5dde01f42d0
Abhijith Anandakrishnan <anandarkishnanabhijith@gmail.com>
2023-04-03 21:43:09 +0530

    Corrected the cash flow api syntax and resolved #1

--- alpha.py
+++ alpha.py
@@ -1,18 +1,32 @@
+import os
 from alpha_vantage.timeseries import TimeSeries
 from dotenv import load_dotenv
 import time
+import requests
 
-ts = TimeSeries(key="API_KEY")
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+
+# ts = TimeSeries(key="API_KEY")
 symbols = ["AAPL", "GOOG", "MSFT", "AMZN"]
 
 for symbol in symbols:
-    cash_flow_data, cash_flow_metadata = ts.cash_flow(symbol=symbol)
+    url = (
+        "https://www.alphavantage.co/query?function=CASH_FLOW&symbol="
+        + symbol
+        + "&apikey="
+        + API_KEY
+    )
+
+    cash_flow_data = requests.get(url).json()
+    # cash_flow_data, cash_flow_metadata = ts.cash_flow(symbol=symbol)
     operating_cash_flow = float(
         cash_flow_data["quarterlyReports"][0]["operatingCashflow"]
     )
+    print(operating_cash_flow)
     if operating_cash_flow > 0:
         print(f"Stock symbol: {symbol}")
         print(f"Operating cash flow: {operating_cash_flow}")
         print("")
 
-time.sleep(15)
+time.sleep(5)
