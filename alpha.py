f6e9d10c9cc97b18695e98e014b4088a942e9b0a
Abhijith Anandakrishnan <anandarkishnanabhijith@gmail.com>
2023-04-04 11:01:37 +0530

    Added exeception to catch the tickers for which cash flow is not available

--- alpha.py
+++ alpha.py
@@ -4,6 +4,7 @@ from alpha_vantage.timeseries import TimeSeries
 from dotenv import load_dotenv
 import time
 import requests
+import logging
 
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
@@ -14,9 +15,10 @@ symbols = []
 
 with open("nasdaq_tickers.csv", "r") as file:
     reader = csv.reader(file)
+    # Skip the header row
+    next(reader, None)
     for row in reader:
         symbols.append(row[0])
-
 
 for symbol in symbols:
     url = (
@@ -28,11 +30,18 @@ for symbol in symbols:
 
     cash_flow_data = requests.get(url).json()
     # cash_flow_data, cash_flow_metadata = ts.cash_flow(symbol=symbol)
-    operating_cash_flow = float(
-        cash_flow_data["quarterlyReports"][0]["operatingCashflow"]
-    )
+    try:
+        cash_flow = cash_flow_data["quarterlyReports"][0]["operatingCashflow"]
+        if cash_flow is not None:
+            operating_cash_flow = float(cash_flow)
+        else:
+            logging.error(f"Cash flow for {symbol} is None")
+            continue
+    except KeyError:
+        logging.error(f"Could not find operating cash flow for {symbol}")
+        continue
     print(operating_cash_flow)
-    if operating_cash_flow > 0:
+    if operating_cash_flow is not None and operating_cash_flow > 0:
         print(f"Stock symbol: {symbol}")
         print(f"Operating cash flow: {operating_cash_flow}")
         print("")
