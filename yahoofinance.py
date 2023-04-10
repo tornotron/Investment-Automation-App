448777ca1f42559180ee4b0fec3a22ee076d0713
Abhijith Anandakrishnan <anandarkishnanabhijith@gmail.com>
2023-04-10 19:52:25 +0530

    Corrected formatting

--- yahoofinance.py
+++ yahoofinance.py
@@ -2,10 +2,9 @@ from yahooquery import Ticker
 import csv
 import sqlite3
 
-conn=sqlite3.connect('cashflowdata.db')
-cur=conn.cursor()
+conn = sqlite3.connect("cashflowdata.db")
+cur = conn.cursor()
 symbols = []
-
 
 
 with open("ticker_test.csv", "r") as file:
@@ -15,22 +14,24 @@ with open("ticker_test.csv", "r") as file:
     for row in reader:
         symbols.append(row[0])
 
-cashflow_values=[]
-recent_cashflow=0
+cashflow_values = []
+recent_cashflow = 0
 
 for symbol in symbols:
-    try:            
-        tickers=Ticker(symbol)     
-        dataframe=tickers.cash_flow(trailing=False)
-        cashflow_values=dataframe['OperatingCashFlow'].values        
-        recent_cashflow=(float(cashflow_values[-1]))
+    try:
+        tickers = Ticker(symbol)
+        dataframe = tickers.cash_flow(trailing=False)
+        cashflow_values = dataframe["OperatingCashFlow"].values
+        recent_cashflow = float(cashflow_values[-1])
         print(recent_cashflow)
-        if(recent_cashflow>0):        
-            cur.execute("INSERT INTO cashflow_table (tickers,cashflow) VALUES (?,?)", (symbol,recent_cashflow))
+        if recent_cashflow > 0:
+            cur.execute(
+                "INSERT INTO cashflow_table (tickers,cashflow) VALUES (?,?)",
+                (symbol, recent_cashflow),
+            )
             conn.commit()
             continue
 
-        
     except TypeError:
         print("data not found")
 conn.close()
