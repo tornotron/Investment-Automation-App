2527d5e07da24333bcf0304e6135b82375291190
kesh-hrishi <hrishikeshajith0@gmail.com>
2023-04-12 03:00:29 +0530

    roce data stored to database

--- databasemanage.py
+++ databasemanage.py
@@ -2,13 +2,24 @@ import sqlite3
 
 conn = sqlite3.connect('cashflowdata.db')
 cur = conn.cursor()
-cur.execute("""CREATE TABLE cashflow_table(
+# cur.execute("""CREATE TABLE cashflow_table(
+#                 tickers TEXT,
+#                 cashflow REAL
+#                 )"""
+#             )
+cur.execute("""CREATE TABLE roce_table(
                 tickers TEXT,
-                cashflow REAL
+                roce REAL
                 )"""
             )
+
+
 # cur.execute("""DROP TABLE cashflow_table
 # """)
+
+# cur.execute("""DROP TABLE roce_table
+# """)
+
 
 conn.commit()
 conn.close()
---
+++ roce.py
@@ -0,0 +1,52 @@
+from yahooquery import Ticker
+import sqlite3
+
+conn = sqlite3.connect("cashflowdata.db")
+cur = conn.cursor()
+
+cur.execute("SELECT tickers from cashflow_table")
+db_rows=cur.fetchall()
+
+
+symbols = [row[0] for row in db_rows]
+
+
+for symbol in symbols:
+    try:
+        tickers = Ticker(symbol)
+
+        dataframe_ebit = tickers.income_statement(trailing=False)
+        ebit_values = dataframe_ebit["EBIT"].values
+        
+        dataframe_totalassets = tickers.balance_sheet(trailing=False)
+        totalassets_values = dataframe_totalassets["TotalAssets"].values
+
+        dataframe_currentliabilities = tickers.balance_sheet(trailing=False)
+        currentliabilities_values = dataframe_currentliabilities["CurrentLiabilities"].values
+
+        
+        recent_values_ebit = float(ebit_values[-1])
+        recent_values_totalassets = float(totalassets_values[-1])
+        recent_values_currentliabilities = float(currentliabilities_values[-1])
+        # print(symbol,":",recent_values_ebit,recent_values_totalassets,recent_values_currentliabilities," ", counter)
+        capitolemployed=recent_values_totalassets-recent_values_currentliabilities
+        roce=(recent_values_ebit/capitolemployed)*100
+        print(symbol,":",roce,"%")
+        if(roce>15):
+            cur.execute("INSERT INTO roce_table (tickers,roce) VALUES (?,?)",(symbol, roce))
+            conn.commit()
+            continue
+
+
+
+        
+
+    except KeyError:
+        print("data not found")   
+
+conn.close() 
+# aapl=Ticker('aapl')
+# earnings_dataframe=aapl.earnings
+
+# print(earnings_dataframe["aapl"]["financialsChart"]
+#       ["yearly"][-1]["revenue"])
